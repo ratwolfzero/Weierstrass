b6a89d817dc39373cf1a75841acdae2cdd8ab8db
Ralf <ratwolf@duck.com>
2025-06-07 19:29:23 +0200

    modified:   experimental/weierstrass_parametric_oscillator_stat.py

--- experimental/weierstrass_parametric_oscillator_stat.py
+++ experimental/weierstrass_parametric_oscillator_stat.py
@@ -211,8 +211,8 @@ def plot_bifurcation_and_lyapunov(h_vals, t, omega0, a, b, n_terms, dt):
 if __name__ == '__main__':
     # Define parameters for the oscillator and Weierstrass function
     omega0 = 1.0
-    a = 0.7
-    b = 9
+    a = 0.5
+    b = 7
     n_terms = 30
     dt = 0.001
     T_max = 500  # Total simulation time
